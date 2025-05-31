e61c4ff528762a65ae55b76f3e84d2affd419218
shireeshageddam <shireeshageddam@gmail.com>
2025-05-31 20:00:29 +0530

    fixed CORS issue for frontent deployment

--- src/main/java/com/example/finance/config/WebConfig.java
+++ src/main/java/com/example/finance/config/WebConfig.java
@@ -11,7 +11,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(@NonNull CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000") // frontend origin
+                .allowedOrigins("https://personal-finance-client-7hqdrmlom-shireeshageddams-projects.vercel.app") // frontend
+                                                                                                                  // origin
                 .allowedMethods("*")
                 .allowedHeaders("*")
                 .allowCredentials(true);
